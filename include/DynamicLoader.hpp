257b72fa219b057c8968d8145f1a47f1ae8ef211
Shun Terabayashi <shunonymous@gmail.com>
2016-05-15 14:12:25 +0900

    - Fix naming - [WIP]Add return type - [WIP]Add arguments of function

--- include/DynamicLoader.hpp
+++ include/DynamicLoader.hpp
@@ -11,22 +11,18 @@
 #include <vector>
 #include <Poco/SharedLibrary.h>
 
-using DlFunc = void (*)(...);
-
 class DynamicLoadLibray
 {
 private:
     std::string Path;
     std::string Symbol;
-    std::map<std::string,void*> Function;
+    std::map<std::string,void*> Functions;
 public:
     void setupLibrary(std::string SharedLibraryName,std::vector<std::string> SymbolNames);
     Poco::SharedLibrary Library;
 
-    inline auto callFunction(std::string FunctionName)
-    {
-	return reinterpret_cast<DlFunc>(Function.at(FunctionName));
-    }
+    template <typename T,typename ... Types>
+    inline auto callFunction(std::string FunctionName);
 
     ~DynamicLoadLibray()
     {
@@ -34,6 +30,13 @@ public:
 	std::cerr << Path << " was unloaded." << std::endl;
     }
 };
+
+template <typename  T = void,typename ... Types>
+inline auto DynamicLoadLibray::callFunction(std::string FunctionName)
+{
+    using DlFunc = T (*)(Types&&...);
+    return reinterpret_cast<DlFunc>(Library.getSymbol(FunctionName));
+}
 
 void DynamicLoadLibray::setupLibrary(std::string SharedLibraryName,std::vector<std::string> SymbolNames)
 {
@@ -53,7 +56,7 @@ void DynamicLoadLibray::setupLibrary(std::string SharedLibraryName,std::vector<s
     }
 
     for(std::string Symbol : SymbolNames)
-	Function.insert(std::make_pair(Symbol,Library.getSymbol(Symbol)));
+	Functions.insert(std::make_pair(Symbol,Library.getSymbol(Symbol)));
 
 } // void DynamicLoadLibray::SetupLibrary(std::string SharedLibraryName,std::vector<std::string> SymbolNames)
 
