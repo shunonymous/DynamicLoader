e89269ddfde1b1fd8613ada2dd1ec3cc841ba5c5
Shun Terabayashi <shunonymous@gmail.com>
2016-06-19 23:43:58 +0900

    - Variable arguments in function - Return type of function

--- include/DynamicLoader.hpp
+++ include/DynamicLoader.hpp
@@ -11,53 +11,130 @@
 #include <vector>
 #include <Poco/SharedLibrary.h>
 
-class DynamicLoadLibray
+namespace DynamicLoader
 {
-private:
-    std::string Path;
-    std::string Symbol;
-    std::map<std::string,void*> Functions;
-public:
-    void setupLibrary(std::string SharedLibraryName,std::vector<std::string> SymbolNames);
-    Poco::SharedLibrary Library;
+    /////////////////////////
+    // Classes declaration //
+    /////////////////////////
+    
+    class DynamicLoadLibray;
 
-    template <typename T,typename ... Types>
-    inline auto callFunction(std::string FunctionName);
+    template <typename T2>
+    class DynamicLoadFunction;
 
-    ~DynamicLoadLibray()
+    ////////////////////////
+    // Classes Definition //
+    ////////////////////////
+
+    class DynamicLoadLibray
     {
-	Library.unload();
-	std::cerr << Path << " was unloaded." << std::endl;
-    }
-};
+    private:
+	std::string Path;
+	std::map<std::string,void*> Functions;
+    public:
+	void loadLibrary(std::string LibraryName);
+	void loadSymbol(std::string SymbolName);
+	void setupLibrary(const std::string LibraryName,std::vector<std::string> SymbolsName);
+	Poco::SharedLibrary Library;
 
-template <typename  T = void,typename ... Types>
-inline auto DynamicLoadLibray::callFunction(std::string FunctionName)
-{
-    using DlFunc = T (*)(Types&&...);
-    return reinterpret_cast<DlFunc>(Library.getSymbol(FunctionName));
-}
+	template <typename T1>
+	auto Function(const std::string FunctionName)
+	{
+	    return DynamicLoadFunction<T1>(Functions.at(FunctionName));
+	}
+    }; // class DynamicLoadLibray
 
-void DynamicLoadLibray::setupLibrary(std::string SharedLibraryName,std::vector<std::string> SymbolNames)
-{
-    // Try to load lib<name><suffix> or <name><suffix>
-    try{
-	Path = "lib" + SharedLibraryName + Poco::SharedLibrary::suffix();
-	Library.load(Path);
-    }
-    catch(std::exception &e)
+    ///////////////////////////////
+    // Define functions in class //
+    ///////////////////////////////
+    
+    template <typename T2>
+    class DynamicLoadFunction
     {
-	Path = SharedLibraryName + Poco::SharedLibrary::suffix();
-	Library.load(Path);
-    }
-    catch(...)
-    {
-	std::cerr << "Error:Cannot load library" << std::endl;
-    }
+    private:
+	std::string FuncName;
+	void* Func;
+    public:
+	template <typename ... Types>
+	auto call(Types ... Args);
 
-    for(std::string Symbol : SymbolNames)
+	DynamicLoadFunction(void* Function)
+	{
+	    Func = Function;
+	}
+
+    }; // class DynamicLoadFunction
+
+    void DynamicLoadLibray::loadLibrary(const std::string LibraryName)
+    {
+	// Try to load lib<name><suffix> or <name><suffix>
+	try{
+	    Path = "lib" + LibraryName + Poco::SharedLibrary::suffix();
+	    Library.load(Path);
+	}
+	catch(std::exception &e)
+	{
+	    Path = LibraryName + Poco::SharedLibrary::suffix();
+	    Library.load(Path);
+	}
+	catch(...)
+	{
+	    std::cerr << "Error:Cannot load library" << std::endl;
+	}
+    } // void DynamicLoadLibray::loadLibrary
+    
+    inline void DynamicLoadLibray::loadSymbol(const std::string Symbol)
+    {
 	Functions.insert(std::make_pair(Symbol,Library.getSymbol(Symbol)));
+    }
 
-} // void DynamicLoadLibray::SetupLibrary(std::string SharedLibraryName,std::vector<std::string> SymbolNames)
+    void DynamicLoadLibray::setupLibrary(const std::string LibraryName,const std::vector<std::string> SymbolsName)
+    {
+	loadLibrary(LibraryName);
+	for(auto&& Symbol:SymbolsName)
+	    loadSymbol(Symbol);
+    } // void DynamicLoadLibray::setupLibrary
 
+    template <typename T2>
+    template <typename ... Types>
+    auto DynamicLoadFunction<T2>::call(Types ... Args)
+    {
+	using DlFunc = T2 (*)(Types...);
+	T2 ReturnVal;
+	DlFunc f;
+	f = reinterpret_cast<DlFunc>(Func);
+	ReturnVal = f(Args...);
+	return ReturnVal;
+    }
+
+    /////////////////////////////
+    // Template specialization //
+    /////////////////////////////
+
+    template <>
+    class DynamicLoadFunction<void>
+    {
+    private:
+	std::string FuncName;
+	void* Func;
+    public:
+	template <typename ... Types>
+	void call(Types ... Args);
+
+	DynamicLoadFunction(void* Function)
+	{
+	    Func = Function;
+	}
+    };
+
+    template <typename ... Types>
+    void DynamicLoadFunction<void>::call(Types ... Args)
+    {
+	using DlFunc = void (*)(Types...);
+	DlFunc f;
+	f = reinterpret_cast<DlFunc>(Func);
+	f(Args...);
+    }
+    
+} // namespace DynamicLoader
 #endif // MIRMIDIVI_DYNAMIC_LOADER_HPP
